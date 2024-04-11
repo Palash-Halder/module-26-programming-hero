9c42f90549c0c56f2c50afeb539d4bbe23bc5805
Palash-Halder <palashhalder.ju@gmail.com>
2024-04-11 14:04:14 +0600

    26-7 Create A Reusable Function For Similar Task

--- 26-6 Extract Common Code Into A Function/scripts/rhombus-area.js
+++ 26-6 Extract Common Code Into A Function/scripts/rhombus-area.js
@@ -1,3 +1,4 @@
+/*
 function calculateRhombusArea(){
     const dimension = getRhombusDimension();
     console.log(dimension);
@@ -9,3 +10,34 @@ function getRhombusDimension() {
     const dimension = parseFloat(dimentionText);
     return dimension;
 }
+
+*/
+
+function calculateRhombusArea(){
+    const dimension1 = getInputValueById('rhombus-dim1');
+    //console.log('base value', dimention1);
+
+    const dimension2 = getInputValueById('rhombus-dim2');
+
+    const area = 0.5 * dimension1 * dimension2;
+    console.log('area of the rhombus is ', area);
+
+    setInnerTextById('rhombus-area', area);
+
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const inputValue = parseFloat(inputValueText);
+    return inputValue;
+}
+
+function setInnerTextById(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
+
+
+
+
